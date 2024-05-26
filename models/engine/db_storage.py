0034c5dedc131bbb74c8644171f35005102c87b1
Aughostinos <augustinos.nabil@gmail.com>
2024-05-27 03:34:43 +0400

    dbengine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -67,6 +67,13 @@ class DBStorage:
 
     def reload(self):
         """create all tables in the database"""
+        from models.state import State
+        from models.city import City
+        from models.user import User
+        from models.place import Place
+        from models.review import Review
+        from models.amenity import Amenity
+        
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
