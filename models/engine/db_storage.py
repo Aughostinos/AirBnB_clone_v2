aeb5ab9b939a43b71f8127d0411c253bd37109cb
Aughostinos <augustinos.nabil@gmail.com>
2024-05-27 03:36:52 +0400

    dbengine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,13 +3,6 @@ from sqlalchemy import create_engine
 import os
 from models.base_model import Base
 from sqlalchemy.orm import scoped_session, sessionmaker
-from models.base_model import Base
-from models.state import State
-from models.city import City
-from models.user import User
-from models.place import Place
-from models.review import Review
-from models.amenity import Amenity
 
 class DBStorage:
     """new engine"""
