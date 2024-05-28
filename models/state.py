983583aa96500cd4d447503449c3291c6b6b6f0e
Aughostinos <augustinos.nabil@gmail.com>
2024-05-29 03:14:45 +0400

    fix

--- models/state.py
+++ models/state.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from models.city import City
 import models
 
 class State(BaseModel, Base):
@@ -10,13 +9,11 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship('City', back_populates='state', cascade='all, delete, delete-orphan')
 
-    @property
-    def cities(self):
-        """Getter attribute cities that returns the list of City instances
-        with state_id equals to the current State.id"""
-        if models.storage_t == 'db':
-            return self.cities
-        else:
+    if models.storage_t != 'db':
+        @property
+        def cities(self):
+            """Getter attribute cities that returns the list of City instances
+            with state_id equals to the current State.id"""
             city_list = []
             for city in models.storage.all(City).values():
                 if city.state_id == self.id:
