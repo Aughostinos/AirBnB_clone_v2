9d3fe9563a06c6cc36c8a7d091f3f3d3b28c7545
Augustinos Abusaif <augustinos.nabil@gmail.com>
2024-06-11 05:40:46 -0700

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,37 +12,37 @@ def do_deploy(archive_path):
     """Distributes an archive to web servers"""
     if not os.path.exists(archive_path):
         return False
-    
-    archine_name = os.path.basename(archive_path)
-    archive_wout_ext = archine_name.split('.')[0]
 
-    tmp_path = "/tmp/{}".format(archine_name)
+    archive_name = os.path.basename(archive_path)
+    archive_wout_ext = archive_name.split('.')[0]
+
+    tmp_path = "/tmp/{}".format(archive_name)
     release_dir = "/data/web_static/releases/{}/".format(archive_wout_ext)
-    current_symplic_link = "/data/web_static/current"
+    current_symlink = "/data/web_static/current"
 
     try:
-        #Upload the archive to the /tmp/ directory of the web server
+        # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, tmp_path)
-        # Create the directory and Uncompress the archive to the folder 
-        # /data/web_static/releases/
-        # <archive filename without extension> on the web server
+
+        # Create the directory and uncompress the archive to the folder
         run("mkdir -p {}".format(release_dir))
-        run("tar -xzf {} -C {}".format(tmp_path, release_dir))         
-        #Delete the archive from the web server
+        run("tar -xzf {} -C {}".format(tmp_path, release_dir))
+
+        # Delete the archive from the web server
         run("rm {}".format(tmp_path))
-        #simplify the structure
-        run("mv{}/web_static/* {}".format(release_dir, release_dir))
-        run("rm -rf {}web_static".format(release_dir))
-        #Delete the symbolic link /data/web_static/current from the web server
-        run("rm -rf {}".format(current_symplic_link))
-        #Create a new the symbolic link 
-        # /data/web_static/current on the web server,
-        # linked to the new version of your code
-        # (/data/web_static/releases/<archive filename without extension>)
-        run("ln -s {}{}".format(release_dir, current_symplic_link))
+
+        # Simplify the structure
+        run("mv {}/web_static/* {}".format(release_dir, release_dir))
+        run("rm -rf {}/web_static".format(release_dir))
+
+        # Delete the symbolic link /data/web_static/current from the web server
+        run("rm -rf {}".format(current_symlink))
+
+        # Create a new symbolic link /data/web_static/current on the web server
+        run("ln -s {} {}".format(release_dir, current_symlink))
 
         return True
 
     except Exception as e:
-        print("deployment failed: {}".format(e))
+        print("Deployment failed: {}".format(e))
         return False
