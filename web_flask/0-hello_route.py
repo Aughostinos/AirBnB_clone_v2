db10d0ba57914dcc86db15a405d6cbf0feb7b092
Aughostinos <augustinos.nabil@gmail.com>
2024-06-27 03:14:08 +0400

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """display Hello HBNB!"""
-    print('Hello HBNB!')
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
