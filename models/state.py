864fe7d6db8914750540ae9b6a40d2dae306739e
Aughostinos <augustinos.nabil@gmail.com>
2024-06-28 01:44:09 +0400

    task7

--- models/state.py
+++ models/state.py
@@ -1,22 +1,26 @@
+#!/usr/bin/python3
+"""
+State module
+"""
+
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
-from models.city import City
-import models
+from os import getenv
+
+HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
+
 
 class State(BaseModel, Base):
-    """State class"""
+    """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', back_populates='state', cascade='all, delete, delete-orphan')
-
-    if models.storage_t != 'db':
+    if HBNB_TYPE_STORAGE == 'db':
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
         @property
         def cities(self):
-            """Getter attribute cities that returns the list of City instances
-            with state_id equals to the current State.id"""
-            city_list = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+            """getter method for cities"""
+            from models import storage
+            from models.city import City
+            return [city for city in storage.all(City).values() if city.state_id == self.id]
