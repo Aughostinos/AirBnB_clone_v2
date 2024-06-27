440fd4417e1abc56b6515a3134705290c0e4e1c9
Aughostinos <augustinos.nabil@gmail.com>
2024-06-28 02:26:13 +0400

    fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-initialize the models package
+Initialize the storage system
 """
 
 from os import getenv
 
+HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
-storage_t = getenv("HBNB_TYPE_STORAGE")
-
-if storage_t == "db":
+if HBNB_TYPE_STORAGE == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
+
 storage.reload()
